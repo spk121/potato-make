50823182446396896ae6b9c34aa1a3b6f43214f1
Mike Gran <spk121@yahoo.com>
2026-04-17 03:57:00 +0000

    Add example files with functions to demonstrate handling names with spaces

---
+++ examples/src/alpha bravo.c
@@ -0,0 +1,6 @@
+/* This file is to test names with spaces */
+
+#include "alpha bravo.h"
+void alpha_bravo() {
+    // This function does nothing
+}
---
+++ examples/src/alpha bravo.h
@@ -0,0 +1,4 @@
+#ifndef ALPHA_BRAVO_H
+#define ALPHA_BRAVO_H
+void alpha_bravo(void);
+#endif
